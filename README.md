b29ec193f500270025649300d99245c9ef68f143
bugph0bia <64964079+bugph0bia@users.noreply.github.com>
2022-03-29 23:39:05 +0900

    細かい修正、README更新

--- README.md
+++ README.md
@@ -14,8 +14,9 @@ Python 3
 
 ## Usage
 ```
-$python csvdiff.py -h
-usage: csvdiff.py [-h] [-e ENCODING] [-p PRIMARYKEY] [-t] [--hide-index] [--hide-keyname] [--hide-value] csv1 csv2
+usage: csvdiff.py [-h] [-e ENCODING] [-p PRIMARY_KEY] [-t] [--excel-style] [--hide-address] [--hide-keyname]
+                  [--hide-value]
+                  csv1 csv2
 
 Output the difference between two CSV files.
 
@@ -27,10 +28,96 @@ optional arguments:
   -h, --help            show this help message and exit
   -e ENCODING, --encoding ENCODING
                         Encoding for CSV files. (default: utf-8)
-  -p PRIMARYKEY, --primarykey PRIMARYKEY
+  -p PRIMARY_KEY, --primary-key PRIMARY_KEY
                         Column number as primary key. (range: 1-N, default: 1)
   -t, --has-title       Treat the first line as a header.
-  --hide-index          Do not print row/column indexes.
+  --excel-style         Print addresses excel A1 style.
+  --hide-address        Do not print row/column addresses.
   --hide-keyname        Do not print row/column key names.
   --hide-value          Do not print difference values.
+```
+
+## Example
+### Sample CSV Files
+a.csv  
+```
+column1,column2,column3
+aaa,1,1,1
+ccc,3,3,3
+ddd,4,4,4
+```
+
+b.csv  
+```
+column1,column2,column3
+aaa,1,1,1
+bbb,2,2,2
+ddd,5,6,7
+```
+
+### Result example
+Simple  
+```
+$ python csvdiff.py a.csv b.csv
++ [R2] > bbb,2,2,2
+- [R2] > ccc,3,3,3
+! [R4,C2 | R4,C2] [ddd,] > 4 | 5
+! [R4,C3 | R4,C3] [ddd,] > 4 | 6
+! [R4,C4 | R4,C4] [ddd,] > 4 | 7
+(diff count: 5)
+```
+
+Handling header row  
+```
+$ python csvdiff.py a.csv b.csv -t
++ [R2] > bbb,2,2,2
+- [R2] > ccc,3,3,3
+! [R3,C2 | R3,C2] [ddd,column2] > 4 | 5
+! [R3,C3 | R3,C3] [ddd,column3] > 4 | 6
+! [R3,C4 | R3,C4] [ddd,] > 4 | 7
+(diff count: 5)
+```
+
+Print addresses excel A1 style
+```
+$ python csvdiff.py a.csv b.csv -t --excel-style
++ [2:2] > bbb,2,2,2
+- [2:2] > ccc,3,3,3
+! [B3 | B3] [ddd,column2] > 4 | 5
+! [C3 | C3] [ddd,column3] > 4 | 6
+! [D3 | D3] [ddd,] > 4 | 7
+(diff count: 5)
+```
+
+Hide addresses  
+```
+$ python csvdiff.py a.csv b.csv -t --hide-address
++ > bbb,2,2,2
+- > ccc,3,3,3
+! [ddd,column2] > 4 | 5
+! [ddd,column3] > 4 | 6
+! [ddd,] > 4 | 7
+(diff count: 5)
+```
+
+Hide key names  
+```
+$ python csvdiff.py a.csv b.csv -t --hide-keyname
++ [R2] > bbb,2,2,2
+- [R2] > ccc,3,3,3
+! [R3,C2 | R3,C2] > 4 | 5
+! [R3,C3 | R3,C3] > 4 | 6
+! [R3,C4 | R3,C4] > 4 | 7
+(diff count: 5)
+```
+
+Hide values  
+```
+$ python csvdiff.py a.csv b.csv -t --hide-value
++ [R2]
+- [R2]
+! [R3,C2 | R3,C2] [ddd,column2]
+! [R3,C3 | R3,C3] [ddd,column3]
+! [R3,C4 | R3,C4] [ddd,]
+(diff count: 5)
 ```
--- csvdiff.py
+++ csvdiff.py
@@ -1,3 +1,4 @@
+import sys
 import csv
 import argparse
 from collections import namedtuple
@@ -5,14 +6,10 @@ from collections import namedtuple
 
 # diff info
 DiffInfo = namedtuple('DiffInfo', [
-    # diff kind (!, -, +)
-    'mark',
-    # row/column indexes of diff
-    'index',
-    # row/column key names of diff
-    'keyname',
-    # values of diff
-    'value',
+    'mark',  # diff kind (!, -, +)
+    'address',  # row/column addresses of diff
+    'keyname',  # row/column key names of diff
+    'value',  # values of diff
 ])
 
 
@@ -22,36 +19,47 @@ def main():
     parser.add_argument('csv1', help='1st CSV file.')
     parser.add_argument('csv2', help='2nd CSV file.')
     parser.add_argument('-e', '--encoding', default='utf-8', help='Encoding for CSV files. (default: utf-8)')
-    parser.add_argument('-p', '--primarykey', type=int, default=1, help='Column number as primary key. (range: 1-N, default: 1)')
+    parser.add_argument('-p', '--primary-key', type=int, default=1, help='Column number as primary key. (range: 1-N, default: 1)')
     parser.add_argument('-t', '--has-title', action='store_true', help='Treat the first line as a header.')
-    parser.add_argument('--hide-index', action='store_true', help='Do not print row/column indexes.')
+    parser.add_argument('--excel-style', action='store_true', help='Print addresses excel A1 style.')
+    parser.add_argument('--hide-address', action='store_true', help='Do not print row/column addresses.')
     parser.add_argument('--hide-keyname', action='store_true', help='Do not print row/column key names.')
     parser.add_argument('--hide-value', action='store_true', help='Do not print difference values.')
     args = parser.parse_args()
-    # correct column number to start with 0
-    args.primarykey -= 1
 
     # read csv
-    csv1, header1 = read_csv(args.csv1, args.encoding, args.primarykey, args.has_title)
-    csv2, header2 = read_csv(args.csv2, args.encoding, args.primarykey, args.has_title)
+    csv1, header1 = read_csv(args.csv1, args.encoding, args.has_title)
+    csv2, header2 = read_csv(args.csv2, args.encoding, args.has_title)
+
     # check column count
     if len(header1) != len(header2):
-        print('error: different column count in CSV files.')
+        print(f'error: different column count in CSV files. (csv1:{len(header1)}, csv2:{len(header2)})', file=sys.stderr)
         return
 
+    # check primary key value
+    if not (0 < args.primary_key <= len(header1)):
+        print(f'error: primary key invalid. (primary key:{args.primary_key}, column count:{len(header1)})', file=sys.stderr)
+        return
+
+    # correct column number to start with 0
+    primary_key = args.primary_key - 1
+
+    # sort by primary key
+    csv1.sort(key=lambda x: x[primary_key])
+    csv2.sort(key=lambda x: x[primary_key])
+
     # get diff info
-    diffs = diff_csv(csv1, header1, csv2, header2, args.primarykey)
+    diffs = diff_csv(csv1, header1, csv2, header2, primary_key, args.excel_style)
     # print result
-    print_diffs(diffs, args.hide_index, args.hide_keyname, args.hide_value)
+    print_diffs(diffs, args.hide_address, args.hide_keyname, args.hide_value)
 
 
-def read_csv(fname: str, encoding: str, primarykey: int, has_header: bool):
-    """Read csv and post-processing
+def read_csv(fname: str, encoding: str, has_header: bool):
+    """Read CSV file
 
     Args:
         fname (str): CSV file.
         encoding (str): encoding for CSV File.
-        primarykey (int): colmn index of primary key.
         has_header (bool): if first row is header then True, else False.
 
     Returns:
@@ -72,14 +80,12 @@ def read_csv(fname: str, encoding: str, primarykey: int, has_header: bool):
     else:
         header = [''] * len(csvdata[0])
 
-    # Sort by primary key
-    csvdata.sort(key=lambda x: x[primarykey])
-
     return csvdata, header
 
 
 def diff_csv(csv1: list[list[str]], header1: list[str],
-             csv2: list[list[str]], header2: list[str], primarykey: int):
+             csv2: list[list[str]], header2: list[str],
+             primary_key: int, excel_style: bool):
     """Diff CSV files.
 
     Args:
@@ -87,10 +93,11 @@ def diff_csv(csv1: list[list[str]], header1: list[str],
         header1 (list[str]): 1st CSV header.
         csv2 (list[list[str]]): 2nd CSV data.
         header2 (list[str]): 2nd CSV header.
-        primarykey (int): colmn index of primary key.
+        primary_key (int): column number of primary key.
+        excel_style (bool): excel A1 style.
 
     Returns:
-        _type_: _description_
+        list[DiffInfo]: list of diff infos.
     """
     diffs = []
 
@@ -100,8 +107,8 @@ def diff_csv(csv1: list[list[str]], header1: list[str],
         row1 = csv1[ri1] if len(csv1) > ri1 else None
         row2 = csv2[ri2] if len(csv2) > ri2 else None
         # get primary key of target row
-        pkey1 = row1[primarykey] if row1 else None
-        pkey2 = row2[primarykey] if row2 else None
+        pkey1 = row1[primary_key] if row1 else None
+        pkey2 = row2[primary_key] if row2 else None
 
         # exit when both CSV data is terminated
         if row1 is None and pkey2 is None:
@@ -112,7 +119,7 @@ def diff_csv(csv1: list[list[str]], header1: list[str],
         elif pkey1 is None:
             diffs.append(DiffInfo(
                 mark='+',
-                index=f'R{str(ri2+1)}',
+                address=make_row_address(ri2, excel_style),
                 keyname='',
                 value=','.join(row2),
             ))
@@ -123,7 +130,7 @@ def diff_csv(csv1: list[list[str]], header1: list[str],
         elif pkey2 is None:
             diffs.append(DiffInfo(
                 mark='-',
-                index=f'R{str(ri1+1)}',
+                address=make_row_address(ri1, excel_style),
                 keyname='',
                 value=','.join(row1),
             ))
@@ -133,7 +140,7 @@ def diff_csv(csv1: list[list[str]], header1: list[str],
         elif pkey1 > pkey2:
             diffs.append(DiffInfo(
                 mark='+',
-                index=f'R{str(ri2+1)}',
+                address=make_row_address(ri2, excel_style),
                 keyname='',
                 value=','.join(row2),
             ))
@@ -143,7 +150,7 @@ def diff_csv(csv1: list[list[str]], header1: list[str],
         elif pkey1 < pkey2:
             diffs.append(DiffInfo(
                 mark='-',
-                index=f'R{str(ri1+1)}',
+                address=make_row_address(ri1, excel_style),
                 keyname='',
                 value=','.join(row1),
             ))
@@ -155,9 +162,9 @@ def diff_csv(csv1: list[list[str]], header1: list[str],
                 if v1 != v2:
                     diffs.append(DiffInfo(
                         mark='!',
-                        index=f'R{ri1+1}|{ri2+1},C{ci+1}',
-                        keyname=f'{pkey1} / {header1[ci]}',
-                        value=f'"{v1}" <> "{v2}"',
+                        address=make_cell_address(ri1, ri2, ci, excel_style),
+                        keyname=f'{pkey1},{header1[ci]}',
+                        value=f'{v1} | {v2}',
                     ))
             ri1 += 1
             ri2 += 1
@@ -165,24 +172,50 @@ def diff_csv(csv1: list[list[str]], header1: list[str],
     return diffs
 
 
-def print_diffs(diffs, hide_index, hide_keyname, hide_value):
+def a1_address(ri, ci):
+    """Make Excel A1 style address from row/column address."""
+    CHR_A = 65  # ascii code of 'A'
+    ALNUM = 26  # number of alphabet
+    if ci >= ALNUM:
+        return chr(CHR_A + (ci // ALNUM)) + chr(CHR_A + (ci % ALNUM)) + str(ri+1)
+    else:
+        return chr(CHR_A + (ci % ALNUM)) + str(ri+1)
+
+
+def make_row_address(ri, excel_style):
+    """Make row address for print."""
+    if excel_style:
+        return f'{ri+1}:{ri+1}'
+    else:
+        return f'R{ri+1}'
+
+
+def make_cell_address(ri1, ri2, ci, excel_style):
+    """Make cell addresses for print."""
+    if excel_style:
+        return f'{a1_address(ri1, ci)} | {a1_address(ri2, ci)}'
+    else:
+        return f'R{ri1+1},C{ci+1} | R{ri2+1},C{ci+1}'
+
+
+def print_diffs(diffs, hide_address, hide_keyname, hide_value):
     """Print diffs.
 
     Args:
         diffs (list[DiffInfo]): list of diff infos.
-        hide_index (bool): if true then do not print indexes.
+        hide_address (bool): if true then do not print addresses.
         hide_keyname (bool): if true then do not print key names.
         hide_value (bool): if true then do not print values.
     """
 
     for diff in diffs:
         pstr = f'{diff.mark} '
-        if not hide_index and diff.index:
-            pstr += f'{diff.index} '
+        if not hide_address and diff.address:
+            pstr += f'[{diff.address}] '
         if not hide_keyname and diff.keyname:
-            pstr += f'<{diff.keyname}> '
+            pstr += f'[{diff.keyname}] '
         if not hide_value and diff.value:
-            pstr += f': {diff.value}'
+            pstr += f'> {diff.value}'
         print(pstr)
     print(f'(diff count: {len(diffs)})')
 
